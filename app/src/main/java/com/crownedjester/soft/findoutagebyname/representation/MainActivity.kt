363c593bc116fa4576d3743e6d1d81b7a676510a
crownedjester <kiyoshisatoshy@gmail.com>
2022-12-30 01:23:44 +0300

    change: change default toast api, clean up activity code

--- app/src/main/java/com/crownedjester/soft/findoutagebyname/representation/MainActivity.kt
+++ app/src/main/java/com/crownedjester/soft/findoutagebyname/representation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.crownedjester.soft.findoutagebyname.representation
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.whenStarted
 import androidx.navigation.NavController
@@ -14,8 +16,11 @@ import com.crownedjester.soft.findoutagebyname.representation.shared_components.
 import com.crownedjester.soft.findoutagebyname.representation.shared_components.UiEvent
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import es.dmoral.toasty.Toasty
 import kotlinx.coroutines.launch
 import kotlin.properties.Delegates
+
+private const val TAG = "MainActivity_Log"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        configToasty()
+
         val navHost =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHost.navController
@@ -37,31 +44,7 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             whenStarted {
                 eventHandlerViewModel.uiEvent.collect { uiEvent ->
-                    when (uiEvent) {
-                        is UiEvent.OnBack -> {
-                            navController.navigateUp()
-                        }
-
-                        is UiEvent.OnNavigate<*> -> {
-                            uiEvent.apply {
-                                if (key.isNullOrEmpty() && arg == null) {
-                                    navController.navigate(routeId)
-                                } else {
-                                    //todo nav with args
-                                }
-                            }
-                        }
-
-                        is UiEvent.ShowToast -> {
-                            Toast.makeText(
-                                this@MainActivity,
-                                uiEvent.message,
-                                Toast.LENGTH_LONG
-                            )
-                                .show()
-                        }
-                    }
-
+                    handleUiEvents(uiEvent)
                 }
             }
 
@@ -79,5 +62,68 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun handleUiEvents(uiEvent: UiEvent) {
+        when (uiEvent) {
+            is UiEvent.OnBack -> {
+                navController.navigateUp()
+                Log.d(TAG, "OnBack event Handled")
+            }
+
+            is UiEvent.OnNavigate<*> -> {
+                uiEvent.apply {
+                    val (routeId, key, arg) = uiEvent
+                    navigate(routeId, key, arg)
+                    Log.d(TAG, "OnNavigate event Handled")
+                }
+            }
+
+            is UiEvent.ShowToast -> {
+                showToast(uiEvent.type, uiEvent.message)
+                Log.d(TAG, "ShowToast event Handled")
+            }
+        }
+    }
+
+    private fun navigate(routeId: Int, argKey: String? = null, arg: Any? = null) {
+        if (argKey.isNullOrEmpty() && arg == null) {
+            navController.navigate(routeId)
+        } else {
+            argKey?.let { key ->
+                navController.navigate(
+                    routeId,
+                    bundleOf(key to arg)
+                )
+            }
+        }
+    }
+
+    private fun showToast(type: UiEvent.ShowToast.ToastType, message: String) {
+        when (type) {
+            UiEvent.ShowToast.ToastType.ERROR ->
+                Toasty.error(applicationContext, message, Toast.LENGTH_LONG).show()
+
+            UiEvent.ShowToast.ToastType.INFO ->
+                Toasty.info(this@MainActivity, message, Toast.LENGTH_LONG).show()
+
+            UiEvent.ShowToast.ToastType.NORMAL ->
+                Toasty.normal(this@MainActivity, message, Toast.LENGTH_LONG).show()
+
+            UiEvent.ShowToast.ToastType.SUCCESS ->
+                Toasty.success(applicationContext, message, Toast.LENGTH_LONG).show()
+
+            UiEvent.ShowToast.ToastType.WARNING ->
+                Toasty.warning(this@MainActivity, message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun configToasty() {
+        Toasty.Config.getInstance()
+            .setTextSize(16)
+            .allowQueue(false)
+            .supportDarkTheme(false)
+            .apply()
+    }
+
 
 }
--- app/src/main/java/com/crownedjester/soft/findoutagebyname/representation/shared_components/UiEvent.kt
+++ app/src/main/java/com/crownedjester/soft/findoutagebyname/representation/shared_components/UiEvent.kt
@@ -8,7 +8,12 @@ sealed class UiEvent {
     data class OnNavigate<T>(val routeId: Int, val key: String? = null, val arg: T? = null) :
         UiEvent()
 
-    data class ShowToast(val message: String) : UiEvent()
+    data class ShowToast(val type: ToastType, val message: String) : UiEvent() {
+        enum class ToastType {
+            ERROR, INFO, NORMAL, SUCCESS, WARNING
+        }
+    }
+
     object OnBack : UiEvent()
 }
 
